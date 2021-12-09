0e189b410c30d00c1c47b5a3c17f23f1835bd0d9
user5-2 <user5-2@citp.energypk.ru>
2021-12-09 13:11:03 +0300

    Добавьте файлы проекта.

---
+++ Viktorina/Viktorina.cpp
@@ -0,0 +1,451 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+	setlocale(LC_ALL, "Russian");
+
+	int i;
+
+	cout << "Вопрос №1" << endl;
+
+	cout << "	В каком городе не работал великий композитор 18-го века Кристоф Виллибальд Глюк?" << endl;
+
+	cout << "1) Милан" << endl;
+
+	cout << "2) Вена" << endl;
+
+	cout << "3) Париж" << endl;
+
+	cout << "4) Берлин" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №2" << endl;
+
+	cout << "	Кто первым доказал периодичность появления комет?" << endl;
+
+	cout << "1) Галилей" << endl;
+
+	cout << "2) Галлей" << endl;
+
+	cout << "3) Кеплер" << endl;
+
+	cout << "4) Коперник" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №3" << endl;
+
+	cout << "	Про какую летнюю погоду говорят ведро ?" << endl;
+
+	cout << "1) Сухая ясная" << endl;
+
+	cout << "2) Прохлданая дождливая " << endl;
+
+	cout << "3) Длительные заморозки" << endl;
+
+	cout << "4) Теплая дождливая" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №4" << endl;
+
+	cout << "	С какой из этих стран Чехия не граничит?" << endl;
+
+	cout << "1) Германия" << endl;
+
+	cout << "2) Венгрия" << endl;
+
+	cout << "3) Австрия" << endl;
+
+	cout << "4) Польша" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №5" << endl;
+
+	cout << "	Где в основном проживают таты?" << endl;
+
+	cout << "1)Татарстан " << endl;
+
+	cout << "2) Дагестан" << endl;
+
+	cout << "3) Башкорстан" << endl;
+
+	cout << "4) Туркменистан" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №6" << endl;
+
+	cout << "	Как, в переводе на русский язык, звучало бы название фильма 'Мимино'?" << endl;
+
+	cout << "1) Медведь" << endl;
+
+	cout << "2) Гора " << endl;
+
+	cout << "3) Любовь" << endl;
+
+	cout << "4) Сокол" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №7" << endl;
+
+	cout << "	Как называется курс парусного судна, совпадающий с направлением ветра?" << endl;
+
+	cout << "1) Бейденвинд" << endl;
+
+	cout << "2) Галфинд" << endl;
+
+	cout << "3) Фордевинд" << endl;
+
+	cout << "4) Бакштаг" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №8" << endl;
+
+	cout << "	На вершине какой горы расположена сорокаметровая статуя Христа, являющаяся символом Рио-де-Жанейро?" << endl;
+
+	cout << "1) Тупунгато" << endl;
+
+	cout << "2) Уаскаран" << endl;
+
+	cout << "3) Корковадо" << endl;
+
+	cout << "4) Ильмани" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №9" << endl;
+
+	cout << "	Какое брюхо, согласно спорной русской пословице, глухо к ученью?" << endl;
+
+	cout << "1) Толстое " << endl;
+
+	cout << "2) Сытое " << endl;
+
+	cout << "3) Тощее" << endl;
+
+	cout << "4) Пустое" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+	cout << " " << endl;
+
+	cout << "Вопрос №10" << endl;
+
+	cout << "	Благодаря какому животному Шурик познакомился с Ниной в к/ф 'Кавказская пленница'?" << endl;
+
+	cout << "1) Верблюд" << endl;
+
+	cout << "2) Конь" << endl;
+
+	cout << "3) Осел" << endl;
+
+	cout << "4) Ежик" << endl;
+
+	cin >> i;
+
+	switch (i) {
+
+	case 1:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 2:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	case 3:
+
+		cout << "Ответ правильный" << endl;
+
+		break;
+
+	case 4:
+
+		cout << "Ответ не правильный" << endl;
+
+		break;
+
+	}
+
+}
